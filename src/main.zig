03ce2b27a70061f79c2d46788d4c3258d16bb273
Riccardo Pasquetto <49592957+codexnabbo@users.noreply.github.com>
2025-06-11 15:39:53 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -17,6 +17,17 @@ pub fn main() !void {
             break;
         } else if (std.mem.eql(u8, cmd,"echo")) {
             try stdout.print("{s}\n", .{pieces.rest()});
+        } else if(std.mem.eql(u8, cmd, "type")) {
+            if(
+                std.mem.eql(u8,pieces.rest(), "exit") or 
+                std.mem.eql(u8,pieces.rest(), "echo") or 
+                std.mem.eql(u8,pieces.rest(), "type")
+            ){
+                try stdout.print("{s} is a shell builtin\n" , .{pieces.rest()});
+            } else {
+                try stdout.print("{s}: not found\n",.{pieces.rest()});
+            }
+            
         } else {
             try stdout.print("{s}: command not found\n", .{user_input});
         }
