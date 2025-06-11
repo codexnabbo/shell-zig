0a95c2de373c03250b142bdfe63582f79a278f8d
Riccardo Pasquetto <49592957+codexnabbo@users.noreply.github.com>
2025-06-11 16:50:50 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -14,20 +14,35 @@ pub fn main() !void {
         const cmd = pieces.first();
 
         if (std.mem.eql(u8, cmd,"exit")) {
-            break;
+            const arg = try std.fmt.parseUnsigned(u8, pieces.rest(), 10);
+            std.process.exit(arg);
         } else if (std.mem.eql(u8, cmd,"echo")) {
             try stdout.print("{s}\n", .{pieces.rest()});
         } else if(std.mem.eql(u8, cmd, "type")) {
+            
+            const allocator = std.heap.page_allocator;
+            const env = try std.process.getEnvMap(allocator);
+            var paths = std.mem.splitScalar(u8, env.get("PATH") orelse return error.OptionalValueIsNull, ':');
             if(
+
                 std.mem.eql(u8,pieces.rest(), "exit") or 
                 std.mem.eql(u8,pieces.rest(), "echo") or 
                 std.mem.eql(u8,pieces.rest(), "type")
             ){
                 try stdout.print("{s} is a shell builtin\n" , .{pieces.rest()});
             } else {
-                try stdout.print("{s}: not found\n",.{pieces.rest()});
+                var findFiles = false;
+                while(paths.next()) |path|{
+                    const abs_path = try std.fmt.allocPrint(allocator, "{s}/{s}", .{path, pieces.rest()});
+                    defer allocator.free(abs_path);
+                    _ = std.fs.openFileAbsolute(abs_path, .{}) catch continue;
+                    try stdout.print("{s} is {s}\n", .{pieces.rest(), abs_path});
+                    findFiles = true;
+                    break;
+                }
+                
+                if(!findFiles) try stdout.print("{s}: not found\n",.{pieces.rest()});
             }
-            
         } else {
             try stdout.print("{s}: command not found\n", .{user_input});
         }
