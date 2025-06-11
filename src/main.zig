8d14d6257c45c3b885fd198a32c2e02dce045273
Riccardo Pasquetto <49592957+codexnabbo@users.noreply.github.com>
2025-06-11 12:40:51 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,20 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
     const stdin = std.io.getStdIn().reader();
-    
+    var buffer: [1024]u8 = undefined;
+
     while (true) {
         try stdout.print("$ ", .{});
-        var buffer: [1024]u8 = undefined;
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        var pieces = std.mem.splitScalar(u8,user_input,' ');
+        const cmd = pieces.first();
 
-        if (std.mem.eql(u8,user_input,"exit 0")) break;
-        try stdout.print("{s}: command not found\n", .{user_input});
+        if (std.mem.eql(u8, cmd,"exit")) {
+            break;
+        } else if (std.mem.eql(u8, cmd,"echo")) {
+            try stdout.print("{s}\n", .{pieces.rest()});
+        } else {
+            try stdout.print("{s}: command not found\n", .{user_input});
+        }
     }
 }
